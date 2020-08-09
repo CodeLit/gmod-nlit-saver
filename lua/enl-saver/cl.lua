394756a1cfe648bf0737ff8966c99b5e180750d5
Neowolk <37398131+Neowolk@users.noreply.github.com>
2020-08-09 23:58:12 +0300

    Распределение

--- lua/enl-saver/cl.lua
+++ lua/enl-saver/cl.lua
@@ -1,1 +1,29 @@
---
+hook.Add('HUDPaint','NL Dulpicator Progress',function()
+  if !ENL.Saver.InProgress or ENL.Saver.Abort then return end
+  local text = 'Сохранятор создает объект... '..math.Round(timer.TimeLeft('NL Duplicator Progress Timer'),1)
+  local txtdata = {text=text,font='DermaLarge',pos={ScrW()-400,ScrH()/15},color=Color(255,255,255)}
+  draw.Text(txtdata)
+  draw.TextShadow(txtdata,2,200)
+  txtdata.text = 'Нажмите R, чтобы отменить создание'
+  txtdata.pos = {ScrW()-510,ScrH()/10}
+  draw.Text(txtdata)
+  draw.TextShadow(txtdata,2,200)
+  if ENL.Saver.InProgress and LocalPlayer():KeyPressed(IN_RELOAD) then
+    ENL.Saver.Abort = true
+  end
+end)
+
+hook.Add('PreDrawHalos', 'ENL Duplicator Draw', function()
+  if table.Count(ENL.Saver.Ents) > 0 then
+    local wep, tool = LocalPlayer():GetActiveWeapon(), LocalPlayer():GetTool()
+    if !IsValid(wep) or wep:GetClass() != 'gmod_tool'
+      or tool.Mode != 'enl_saver' then return end
+  
+    local haloEnts = {}
+    for ent, bool in pairs(ENL.Saver.Ents) do
+      if bool and IsValid(ent) then table.insert(haloEnts, ent)
+      else ENL.Saver.Ents[ent] = nil end
+    end
+    halo.Add(haloEnts, Color(0, 255, 0), 10, 10, 1)
+  end
+end)
--- lua/weapons/gmod_tool/stools/enl_saver.lua
+++ lua/weapons/gmod_tool/stools/enl_saver.lua
@@ -1,8 +1,6 @@
 TOOL.Category = 'Construction'
 TOOL.Name = '#tool.enl_saver.name'
 
-local function GetEntID(ent) return ent:GetCreationID() end
-
 if SERVER then
 
 	function TOOL:LeftClick(tr)
@@ -17,7 +15,11 @@ if SERVER then
 
 	function TOOL:RightClick(tr)
 	  return self:LeftClick(tr)
-	end
+  end
+  
+  function TOOL:Reload()
+    return true
+  end
 
   local firstEnts = {}
 
@@ -181,21 +183,6 @@ elseif CLIENT then
     end
   end
 
-  hook.Add('HUDPaint','NL Dulpicator Progress',function()
-    if !ENL.Saver.InProgress or ENL.Saver.Abort then return end
-    local text = 'Сохранятор создает объект... '..math.Round(timer.TimeLeft('NL Duplicator Progress Timer'),1)
-    local txtdata = {text=text,font='DermaLarge',pos={ScrW()-400,ScrH()/15},color=Color(255,255,255)}
-    draw.Text(txtdata)
-    draw.TextShadow(txtdata,2,200)
-    txtdata.text = 'Нажмите R, чтобы отменить создание'
-    txtdata.pos = {ScrW()-510,ScrH()/10}
-    draw.Text(txtdata)
-    draw.TextShadow(txtdata,2,200)
-    if ENL.Saver.InProgress and LocalPlayer():KeyPressed(IN_RELOAD) then
-      ENL.Saver.Abort = true
-    end
-  end)
-
   function TOOL:BuildCPanel()
 
     local function AddButton(btn)
@@ -322,23 +309,17 @@ elseif CLIENT then
     end
   end)
 
-  function TOOL:Reload(tr)
+  -- function TOOL:LeftClick(tr)
+	--   return true
+  -- end
+  
+  -- function TOOL:RightClick()
+	--   return self:LeftClick(tr)
+  -- end
+  
+  function TOOL:Reload()
     ENL.Saver.Ents = {}
+    return true
   end
-
-  hook.Add('PreDrawHalos', 'ENL Duplicator Draw', function()
-    if table.Count(ENL.Saver.Ents) > 0 then
-      local wep, tool = LocalPlayer():GetActiveWeapon(), LocalPlayer():GetTool()
-	    if !IsValid(wep) or wep:GetClass() != 'gmod_tool'
-	      or tool.Mode != 'enl_saver' then return end
-    
-	    local haloEnts = {}
-	    for ent, bool in pairs(ENL.Saver.Ents) do
-	      if bool and IsValid(ent) then table.insert(haloEnts, ent)
-	      else ENL.Saver.Ents[ent] = nil end
-	    end
-	    halo.Add(haloEnts, Color(0, 255, 0), 10, 10, 1)
-		end
-  end)
 
 end
